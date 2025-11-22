460a99a09a2d330bc01ad52eceb94476836dd1d5
mayada-shaaban <Mayada@i-legal.me>
2025-11-22 15:09:41 +0200

    feat: implement initial application layout with global sidebar navigation and new pages for credit cards and investments

---
+++ components/global/icons.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export function CreditCards() {
+	return (
+		<Image
+			src={'/icons/credit-cards.svg'}
+			alt='[ ]'
+			width={36}
+			height={36}
+		/>
+	)
+}
